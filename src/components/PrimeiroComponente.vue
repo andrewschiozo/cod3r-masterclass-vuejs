5921d3b666c9320916833e490e619dfe68488598
Andrews <andrewschiozo@gmail.com>
2024-11-22 16:44:10 -0300

    style: estilização do componente

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -3,3 +3,24 @@
         <h1>Primeiro Componente</h1>
     </div>
 </template>
+
+<style scoped>
+/* O scoped aplica a estilização apenas para esse componente */
+ 
+div {
+    background-color: #0e7e28;
+    padding: 20px;
+    margin: 30px;
+    border-radius: 5px;
+}
+
+div:hover {
+    background-color: #13b939;
+}
+
+h1 {
+    color: #fff;
+    margin-bottom: 10px;
+}
+
+</style>
