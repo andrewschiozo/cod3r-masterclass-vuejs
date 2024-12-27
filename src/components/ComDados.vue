d24fbf5eaa943fe99bc2cdde4d454685461d2d2e
Andrews <andrewschiozo@gmail.com>
2024-12-27 18:40:11 -0300

    style: estilização global dos componentes

--- src/components/ComDados.vue
+++ src/components/ComDados.vue
@@ -1,24 +1,9 @@
 <template>
-  <div>
+  <div class="componente">
     <h1>Componente com dados</h1>
     <p>{{ nomeProduto }} - {{ codigoProduto }}</p>
   </div>
 </template>
-
-<style scoped>
-    div{
-        text-align: center;
-        border: 2px solid rgb(10, 69, 4);
-        background-color: #050d04;
-        border-radius: 15px;
-        margin: 30px;
-        padding: 10px;
-    }
-
-    p {
-        font-size: 1.2em;
-    }
-</style>
 
 <!-- Ao utilizar o setup, o script se comporta como composition api -->
 <script setup>
