f7931bc69b00e2a6fcbb47e8bdaf895a78e047df
Andrews <andrewschiozo@gmail.com>
2025-01-03 17:41:23 -0300

    feat: emitindo evento, enviando o objeto inteiro

---
+++ src/components/EmiteEvento.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="componente">
+        <h1>Componente Emite Evento</h1>
+        <p>{{ item }} | (R$ {{ preco.toFixed(2) }})</p>
+        <button @click="adicioarAoCarrinho">Adicionar ao carrinho</button>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps, defineEmits } from 'vue'
+
+    const props = defineProps({
+        item: String,
+        preco: Number
+    })
+
+    const emit = defineEmits(['adicionarAoCarrinho'])
+
+    function adicioarAoCarrinho() {
+        emit('adicionarAoCarrinho', props)
+    }
+</script>
