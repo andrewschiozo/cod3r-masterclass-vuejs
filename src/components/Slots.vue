a525732ae08cc35fc8137e98d502dfd807887a13
Andrews <andrewschiozo@gmail.com>
2025-01-06 11:41:59 -0300

    fix: correção do name do slot

--- src/components/Slots.vue
+++ src/components/Slots.vue
@@ -2,7 +2,7 @@
     <div class="componente">
         <h1>Componente com Slots</h1>
         <button>
-            <slot name="icone" /> <slot name="texo" />
+            <slot name="icone" /> <slot name="texto" />
         </button>
     </div>
 </template>
