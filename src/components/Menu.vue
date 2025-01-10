c4d4bdcfcdb2d2a659dbd94694d51698468d04e9
Andrews <andrewschiozo@gmail.com>
2025-01-10 16:32:02 -0300

    feat: Componente do menu

---
+++ src/components/Menu.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav>
+        <div class="componente">
+        <h1>Componente com VueRouter</h1>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/link1">Link 1</RouterLink>
+        <RouterLink to="/link2">Link 2</RouterLink>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  nav a {
+    margin-right: 10px;
+    text-decoration: none;
+    color: #fff;
+    font-weight: bold;
+  }
+  nav a.router-link-active {
+    color: #42b983;
+  }
+</style>
