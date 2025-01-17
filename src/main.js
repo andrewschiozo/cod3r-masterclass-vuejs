9f73d2c2172aa4a570cc61e6eb70f33a944d66b3
Andrews <andrewschiozo@gmail.com>
2025-01-17 16:06:52 -0300

    feat

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import router from './router'
+import { createPinia } from 'pinia'
+import { useAuth } from '@/stores/auth'
 
 import App from './App.vue'
-import { createPinia } from 'pinia'
 
+const app = createApp(App)
 const pinia = createPinia()
+// pinia.use(({ store }) => { store.router = markRaw(router) })
 
-createApp(App)
-    .use(router)
-    .use(pinia)
-    .mount('#app')
+app.use(pinia)
+app.use(router)
+
+console.log('main')
+
+if(localStorage.getItem('token')) {
+    //iife
+    (async () => {
+        const auth = useAuth()
+        try{
+            auth.setIsAuth(true)
+            await auth.checkToken()
+        } catch (error) {
+            auth.logout()
+        }
+    })()
+}
+    
+app.mount('#app')
