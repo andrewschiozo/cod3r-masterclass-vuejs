8d30218b8f22a27674e756657945a312bc6acf0e
Andrews <andrewschiozo@gmail.com>
2025-01-17 09:41:04 -0300

    feat: uso do pinia no app

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import router from './router'
+
 import App from './App.vue'
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
 
 createApp(App)
     .use(router)
+    .use(pinia)
     .mount('#app')
