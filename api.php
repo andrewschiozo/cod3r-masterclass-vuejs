32d71d65a4593113d8758b58031449b0d9d65477
Andrews <andrewschiozo@gmail.com>
2025-01-17 16:04:41 -0300

    refactor: allow all methods cors

--- api.php
+++ api.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
 header('Content-Type: application/json');
 
 define('INPUT_DATA', json_decode(file_get_contents('php://input')) ?? json_decode('{}'));
