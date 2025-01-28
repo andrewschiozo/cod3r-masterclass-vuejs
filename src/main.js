e2e5ca3ca31f7ce8ba3523819ba47c570de648ee
Andrews <andrewschiozo@gmail.com>
2025-01-28 13:53:47 -0300

    feat: notificação toast

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createApp, markRaw } from 'vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import { useAuth } from '@/stores/auth'
+import Vue3Toastify from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
 import App from './App.vue'
 
@@ -13,6 +15,7 @@ const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+app.use(Vue3Toastify, { autoClose: 3000 })
 
 if(localStorage.getItem('token')) {
     //iife
