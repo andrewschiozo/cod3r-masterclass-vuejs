33705def9df5896343e384d3703586e368020a73
Andrews <andrewschiozo@gmail.com>
2025-01-30 17:49:36 -0300

    feat: componente Tabela do pedido

---
+++ src/components/Pedido/PedidoRow.vue
@@ -0,0 +1,23 @@
+<template>
+    <tr>
+        <td>{{pedido.order}}</td>
+        <td>{{pedido.client}}</td>
+        <td>{{pedido.amount}}</td>
+        <td>{{pedido.items}}</td>
+        <td><button @click="emit('editarPedido', pedido)">Editar</button> <button @click="emit('removerPedido', pedido)">Remover</button></td>
+    </tr>
+</template>
+
+<script setup>
+    defineProps({ pedido: Object })
+    const emit = defineEmits(['editarPedido', 'removerPedido'])
+</script>
+
+<style scoped>
+    td {
+        padding: 5px
+    }
+    td:nth-child(2) {
+        text-align: left
+    }
+</style>
---
+++ src/components/Pedido/PedidoTable.vue
@@ -0,0 +1,47 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Cliente</th>
+                <th>Vl. Total</th>
+                <th>Qtd. Itens</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <PedidoRow v-for="pedido in pedidos" :key="pedido.order" :pedido="pedido" @removerPedido="removerPedido" @editarPedido="editarPedido"/>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+    // defineProps({ pedidos: Array })
+    import PedidoRow from './PedidoRow.vue'
+    import router from '@/router'
+
+    const props = defineProps({ pedidos: Array })
+
+    function removerPedido(pedido) {
+        props.pedidos.splice(props.pedidos.indexOf(pedido), 1)
+    }
+    
+    function editarPedido(pedido) {
+        router.push({ name: 'pedidoform' })
+    }
+</script>
+
+<style scoped>
+    table {
+        border-collapse: collapse;
+        width: 100%;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    thead {
+        border-bottom: 2px solid #FFF;
+    }
+    th:nth-child(2) {
+        text-align: left
+    }
+</style>
